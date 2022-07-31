701ae8849a50f1ab37aa8a12738dbe571a0010df
gtchappy <2425172709@qq.com>
2022-07-31 23:15:10 +0800

    deploy

---
+++ precache-manifest.9b4a7540f8a848bf0104cad0dbf9458e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a829d43ae4956afaf0c4",
+    "url": "/tally-book-website/css/app.4d179a40.css"
+  },
+  {
+    "revision": "b8ccc7be77acb85c9e3947200c47d3d9",
+    "url": "/tally-book-website/index.html"
+  },
+  {
+    "revision": "a829d43ae4956afaf0c4",
+    "url": "/tally-book-website/js/app.df91b61b.js"
+  },
+  {
+    "revision": "b09f9c6e4d1220ac9412",
+    "url": "/tally-book-website/js/chunk-vendors.866073b2.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/tally-book-website/manifest.json"
+  },
+  {
+    "revision": "bfb690873c3683829576a1f906d3a763",
+    "url": "/tally-book-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tally-book-website/robots.txt"
+  }
+]);
